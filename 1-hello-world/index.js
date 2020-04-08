15deb46bfe1c101040df25be40d5d7dc34ae6872
Bao Dien <baodien1405@gmail.com>
2020-04-08 22:32:06 +0700

    Update some feature

--- 1-hello-world/index.js
+++ 1-hello-world/index.js
@@ -2,12 +2,47 @@ var express = require('express');
 var app = express();
 var port = 3000;
 
-app.get('/', function(require, response) {
-    response.send('<h1>Hello world</h1>');
+app.set('view engine', 'pug');
+app.set('views', './views');
+
+app.use(express.json()) // for parsing application/json
+app.use(express.urlencoded({ extended: true })) // for parsing application/x-www-form-urlencoded
+
+var users = [
+    {id: 1, name: 'Điền'},
+    {id: 2, name: 'Thi'}
+];
+
+app.get('/', function(req, res) {
+    res.render('index', {
+        name: 'Cáp Bảo Điền'
+    });
 });
 
-app.get('/users', function(require, response) {
-    response.send('User list');
+app.get('/users', function(req, res) {
+    res.render('users/index', {
+        users: users
+    });
+});
+
+app.get('/users/search', function(req, res) {
+    var q = req.query.q;
+    var matchedUsers = users.filter(function(user) {
+        return user.name.toLowerCase().indexOf(q.toLowerCase()) !== -1;
+    });
+
+    res.render('users/index', {
+        users: matchedUsers
+    });
+});
+
+app.get('/users/create', function(req, res) {
+    res.render('users/create');
+});
+
+app.post('/users/create', function(req, res) {
+    users.push(req.body);
+    res.redirect('/users');
 });
 
 app.listen(port, function() {
